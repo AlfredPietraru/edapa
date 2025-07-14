af3559d0605c30f4fc6235078352ad74becd101a
AlfredPietraru <alfred.andrei@yahoo.com>
2025-07-14 08:40:27 +0300

    bug fix test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PATH_TO_YAMLS=/home/alf/edapa/other/yolov5/yamls
+PATH_TO_YAMLS=/home/alf/edapa/new/yamls
 yaml_files=($(cd ${PATH_TO_YAMLS} &&  ls *.yaml))
 
 for yaml_file in "${yaml_files[@]}"; do 

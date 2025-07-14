89aece84198bd1ee9526579bb1b79f4fb43f17ad
AlfredPietraru <alfred.andrei@yahoo.com>
2025-07-14 09:47:04 +0300

    fixed pipeline independently

--- pipeline.sh
+++ pipeline.sh
@@ -16,7 +16,6 @@ MODEL_NAME=$(basename $YAML .yaml)
 SAVE_MODEL_PATH=${MODEL_NAME}_saved_model
 MODEL=${MODEL_NAME}.pt
 
-
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
@@ -24,11 +23,10 @@ eval "$(pyenv init -)"
 pyenv shell 3.10.1
 source my_venv/bin/activate
 
-if [ ! -d ${SAVE_MODEL_PATH} ]
-then
-    mkdir ${SAVE_MODEL_PATH}
-fi
-
+# if [ ! -d ${SAVE_MODEL_PATH} ]
+# then
+#     mkdir ${SAVE_MODEL_PATH}
+# fi
 
 # yolo detect train \
 # imgsz=${SIZE} \
@@ -37,32 +35,24 @@ fi
 # data=$DATASET_YAML \
 # model=$YAML
 
-
 # mv runs/detect/train/weights/best.pt ./${MODEL}
 
 # yolo export \
+# imgsz=${SIZE} \
 # model=${MODEL} \
 # format=tflite \
-# imgsz=$SIZE \
 # optimize=True \
 # int8=True
 
+# mv runs/detect/train/results.csv ${SAVE_MODEL_PATH}
+# rm -rf runs
 
-mv runs/detect/train/results.csv ${SAVE_MODEL_PATH}
-edgetpu_compiler ${SAVE_MODEL_PATH}/${MODEL_NAME}_full_integer_quant.tflite
-cat ${MODEL_NAME}_full_integer_quant_edgetpu.log
-mv ${MODEL_NAME}_full_integer_quant_edgetpu.log  ${MODEL_NAME}_full_integer_quant_edgetpu.tflite ${SAVE_MODEL_PATH}
+# edgetpu_compiler ${SAVE_MODEL_PATH}/${MODEL_NAME}_full_integer_quant.tflite
+# cat ${MODEL_NAME}_full_integer_quant_edgetpu.log
+# mv ${MODEL_NAME}.onnx ${MODEL_NAME}_full_integer_quant_edgetpu.log  ${MODEL_NAME}_full_integer_quant_edgetpu.tflite ${SAVE_MODEL_PATH}
 
-# python detect.py \
-# --weights ${SAVE_MODEL_PATH}/${MODEL_NAME}-int8_edgetpu.tflite \
-# --source $DATASET_DETECT \
-# --data $DATASET_YAML \
-# --img $SIZE \
-# --conf-thres 0.25 \
-# --iou-thres 0.1 \
-# --project ${SAVE_MODEL_PATH} \
-# --name detect_exp 
-
-# mv ${SAVE_MODEL_PATH}/train_exp/weights/best.pt ./${MODEL}
-# rm -rf ${MODEL_NAME}_saved_model
-# echo "SE EXECUTA CU SUCCES TOATE COMENZILE"
+yolo detect val \
+imgsz=${SIZE}
+model=${SAVE_MODEL_PATH}/${MODEL_NAME}_full_integer_quant_edgetpu.tflite
+source=$DATASET_DETECT \
+data=$DATASET_YAML \

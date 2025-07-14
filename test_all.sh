b38fa6b6b4cb116ebe460a0a8a7d9cbb562aa13e
AlfredPietraru <alfred.andrei@yahoo.com>
2025-07-14 08:39:10 +0300

    yamls + test_all script

--- test_all.sh
+++ test_all.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-PATH_TO_YAMLS=/home/alf/edapa/other/yolov5/models
+PATH_TO_YAMLS=/home/alf/edapa/other/yolov5/yamls
 yaml_files=($(cd ${PATH_TO_YAMLS} &&  ls *.yaml))
 
 for yaml_file in "${yaml_files[@]}"; do 
-    var=$(echo $yaml_file | cut -d'.' -f1)
-    echo Try to work with $var.yaml configure file.
-    ./pipeline.sh $var
+    ./pipeline.sh yamls/$yaml_file
 done

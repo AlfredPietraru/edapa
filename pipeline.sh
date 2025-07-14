abf7109e3a6a0466a7b8caacecc2e270439f5aa4
AlfredPietraru <alfred.andrei@yahoo.com>
2025-07-14 09:30:50 +0300

    generate.py deleted, no longer necessary, rebuild after loss

--- pipeline.sh
+++ pipeline.sh
@@ -2,7 +2,7 @@
 
 
 # CLEAR_ENABLED=$1
-MODEL_NAME=$1
+YAML=$1
 SIZE=1024
 # DATASET_YAML=/home/alf/edapa/other/datasets/VisDrone/VisDrone.yaml
 # DATASET_DETECT=/home/alf/edapa/other/datasets/VisDrone/VisDrone2019-DET-val/images
@@ -12,7 +12,8 @@ DATASET_DETECT=/home/alf/edapa/other/datasets/coco8/images/val
 
 # DATASET_YAML=/home/alf/edapa/other/datasets/VisDroneLonely/VisDroneLonely.yaml
 # DATASET_DETECT=/home/alf/edapa/other/datasets/VisDroneLonely/VisDrone2019-DET-val/images
-SAVE_MODEL_PATH=dir_${MODEL_NAME}
+MODEL_NAME=$(basename $YAML .yaml)
+SAVE_MODEL_PATH=${MODEL_NAME}_saved_model
 MODEL=${MODEL_NAME}.pt
 
 
@@ -20,34 +21,37 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
-pyenv shell 3.9.17
-source venv/bin/activate
+pyenv shell 3.10.1
+source my_venv/bin/activate
+
 if [ ! -d ${SAVE_MODEL_PATH} ]
 then
     mkdir ${SAVE_MODEL_PATH}
 fi
 
 
-# --weights ${MODEL} \
-
-yolo detect train  \
-imgsz=$SIZE \
-batch=4 \
-epochs=2 \
-data=$DATASET_YAML \
-cfg=models/${MODEL_NAME}.yaml \
-save-period=2 \
-project=${SAVE_MODEL_PATH} \
-name=train_exp 
-
-# mv ${SAVE_MODEL_PATH}/train_exp/weights/best.pt ./${MODEL}
-
-# python export.py --weights ${MODEL} --include tflite --int8 --img $SIZE
+# yolo detect train \
+# imgsz=${SIZE} \
+# batch=4 \
+# epochs=2 \
+# data=$DATASET_YAML \
+# model=$YAML
 
 
-# edgetpu_compiler ${MODEL_NAME}-int8.tflite
-# cat ${MODEL_NAME}-int8_edgetpu.log
-# mv ${MODEL_NAME}-int8.tflite ${MODEL_NAME}-int8_edgetpu.log ${MODEL_NAME}-int8_edgetpu.tflite ${SAVE_MODEL_PATH}
+# mv runs/detect/train/weights/best.pt ./${MODEL}
+
+# yolo export \
+# model=${MODEL} \
+# format=tflite \
+# imgsz=$SIZE \
+# optimize=True \
+# int8=True
+
+
+mv runs/detect/train/results.csv ${SAVE_MODEL_PATH}
+edgetpu_compiler ${SAVE_MODEL_PATH}/${MODEL_NAME}_full_integer_quant.tflite
+cat ${MODEL_NAME}_full_integer_quant_edgetpu.log
+mv ${MODEL_NAME}_full_integer_quant_edgetpu.log  ${MODEL_NAME}_full_integer_quant_edgetpu.tflite ${SAVE_MODEL_PATH}
 
 # python detect.py \
 # --weights ${SAVE_MODEL_PATH}/${MODEL_NAME}-int8_edgetpu.tflite \
